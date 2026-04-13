78b74fbfe25a25ca89108cb9d53b50d9f20d8c3d
Sehasna2004 <dinuli.ferdinando@gmail.com>
2026-04-13 13:12:30 +0530

    Initial Spring Boot setup for PawMart

---
+++ src/test/java/com/pawmart/petcare_backend/PetcareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawmart.petcare_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetcareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
